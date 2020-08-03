a8871cdef6c336093cc500117ee152c4fced9dca
nagamoridaiki <no.swea1017@gmail.com>
2020-08-03 13:07:34 +0900

    アカウント登録ページ作成

--- chat/nuxt.config.js
+++ chat/nuxt.config.js
@@ -22,7 +22,9 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      /*アイコンフォントを使用する*/
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
     ]
   },
   /*
@@ -35,7 +37,7 @@ export default {
   */
  plugins: ['~/plugins/firebase',
             '~/plugins/auth',
-            
+
           ],
   /*
   ** Auto import components
